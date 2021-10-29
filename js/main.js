8c0ae78783d24621aa7aa13ec005ad596827743c
pranitapaudel <pranitapaudel67@gmail.com>
2021-10-29 12:25:57 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+  
+  scrollToTopBtn.addEventListener("click", scrollToTop)
+  function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+
+  }
